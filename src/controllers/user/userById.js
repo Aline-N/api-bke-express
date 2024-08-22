9acf55d991936ce51480ec0bfeceb6c88670384f
Aline Nunes <devenirseul@gmail.com>
2024-08-21 22:04:13 -0300

    add userById

--- src/controllers/user/userById.js
+++ src/controllers/user/userById.js
@@ -1,5 +1,19 @@
+import { getById } from "../../models/userModel.js"
+
 const userById = (req, res) => {
-    res.json({message: "Rota GET /user"})
+    const {id} = req.params
+    
+    const user = await getById(+id)
+    
+    if(user)
+        return res.json({
+        message: "User get by id",
+        user
+    })
+    else
+        return res.status(404).json({
+            error: "Usuário não encontrado"
+        })
 }
 
 export default userById
